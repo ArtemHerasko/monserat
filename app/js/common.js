c9f7ac8135a5cf09a400877087743f588fe1d75d
ArtemHerasko <gerasko.a@gmail.com>
2017-05-03 14:03:43 +0300

    Fixed header

--- app/js/common.js
+++ app/js/common.js
@@ -8,6 +8,16 @@ $(function() {
 		},
 		'extensions': ['theme-dark', 'fx-listitems-slide']
 	});
+
+	var $menu = $("#top-menu");
+
+        $(window).scroll(function(){
+            if ( $(this).scrollTop() > 180 && $menu.hasClass("default") ){
+                $menu.removeClass("default").addClass("fixed-header");
+            } else if($(this).scrollTop() <= 100 && $menu.hasClass("fixed-header")) {
+                $menu.removeClass("fixed-header").addClass("default");
+            }
+        });
 	
 	$(".accordeon dd").hide().prev().click(function() {
 	$(this).parents(".accordeon").find("dd").not(this).slideUp(150).prev().removeClass("active");
@@ -21,16 +31,10 @@ $(function() {
 		$('.hamburger').removeClass('is-active');
 	});
 	$(".carousel-slider").owlCarousel({
-		nav: true,
-		navText: ['<span class="left-arrow"></span>','<span class="right-arrow"></span>'],
-		dots: true,
-		dotsEach:true,
-		smartSpeed:700,
-		 responsive:{
-        0:{
-            items:1
-        }
-    }
-
+		
+    items:1,
+    singleItem: true,
+    center: true,
+    loop: true
 	});
 });
